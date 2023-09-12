b485476e77d90538db164d5a08f1947de8421afa
pixelskull <pascal.schoenthier@th-koeln.de>
2023-09-12 23:22:29 +0200

    Added Reconnect to PubSubClient and auto formatted

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,58 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include "config.h"
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
 
 // MQTT Server settings
 const int mqttPort = 1883;
-const char* mqttTopic = "locations";
+const char *mqttTopic = "locations";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+void connectClient() {
+  client.setServer(mqttServer, mqttPort);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
+
 void setup() {
-    // Start serial
-    Serial.begin(115200);
+  // Start serial
+  Serial.begin(115200);
 
-    // Connect to WiFi
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
-    }
-    Serial.println("Connected to WiFi");
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 
-    client.setServer(mqttServer, mqttPort);
-
-    while (!client.connected()) {
-        Serial.println("Connecting to MQTT...");
-
-        if (client.connect("ESP32Client")) {
-            Serial.println("connected");
-        } else {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
-            delay(2000);
-        }
-    }
+  connectClient();
 }
 
 void loop() {
-    // Create JSON with dummy values
-    String jsonData = "{\"imei\":\"123456789\",\"latitude\":\"50.1109\",\"longitude\":\"8.6821\"}";
+  if (!client.connected()) {
+    connectClient();
+  }
 
-    // Send data over MQTT
-    client.publish(mqttTopic, jsonData.c_str());
+  // Create JSON with dummy values
+  String jsonData = "{\"imei\":\"123456789\",\"latitude\":\"50.1109\","
+                    "\"longitude\":\"8.6821\"}";
 
-    // Wait before sending again
-    delay(1000);
+  // Send data over MQTT
+  client.publish(mqttTopic, jsonData.c_str());
+  Serial.println("topic sendet...");
+
+  // Wait before sending again
+  delay(1000);
 }
-
